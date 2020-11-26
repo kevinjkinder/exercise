95ea7b91a201e2614cc541d274d1257ae2518ce5
kemaster <kevinjkinder@gmail.com>
2020-11-26 19:27:40 -0300

    Slice
    .

--- src/js/table.js
+++ src/js/table.js
@@ -36,13 +36,9 @@ export function sortBy (arr, prop) {
 // return array with subarrays of "size" length
 export function chunk (array, size) {
   const chunkedArr = []
-  for (let i = 0; i < array.length; i++) {
-    const last = chunkedArr[chunkedArr.length - 1]
-    if (!last || last.length === size) {
-      chunkedArr.push([array[i]])
-    } else {
-      last.push(array[i])
-    }
+  for (let i = 0; i < array.length; i += size) {
+    const chunk = array.slice(i, i + size)
+    chunkedArr.push(chunk)
   }
   return chunkedArr
 }
